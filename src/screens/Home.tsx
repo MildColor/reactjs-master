3e3ec6991d8757bfd2d0e86414eef168684f475f
MildColor <96479626+MildColor@users.noreply.github.com>
2022-11-08 17:01:50 +0900

    #4.8 useSearchParams

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,8 +1,15 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 import { users } from "../db";
 
 function Home() {
+  const [readSearchParams, setSearchParams] = useSearchParams();
+  setTimeout(() => {
+    setSearchParams({
+      day: "today",
+      tomorrow: "123",
+    });
+  });
   return (
     <div>
       <h1>Users</h1>
